888a03a126a68489af8c08d79d69b490d30f1c30
Muhammad Hamza Sohail <hamza.sohail29@gmail.com>
2024-02-20 15:22:00 -0500

    Setup: Firebase & Cors

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
-const app = express();
+const admin = require("firebase-admin");
+const cors = require("cors");
+const credentials = require("./key.json");
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials),
+  });
+  const db = admin.firestore();
+  
+  const app = express();
+  app.use(cors());
+
+
 
 app.get("/home", (req, res) => {
 	res.send("Hello we are a Team")
