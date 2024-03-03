31f7243f0265785d9b34e4dd68495cb9d212f6df
Cody Villareal <c0dyv@my.yorku.ca>
2024-03-03 01:14:25 -0500

    implmented Sign In using SDK

--- routes/user/signIn.js
+++ routes/user/signIn.js
@@ -1,23 +1,52 @@
 const express = require('express');
 const router = express.Router();
+import {initializeApp, getAuth, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js';
+import {getDatabase, set, ref} 'https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js';
+import {getAuth, createUserWithEmailAndPassword} from 'https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js';
 
- // intialize the DB of the user
-router.post('/users', async (req, res) => {
-    const { email, password, name } = req.body;
-  
-    try {
-      const userRecord = await req.app.locals.admin.auth().createUser({
-       
-      });
-  
-     
-    } catch (error) {
-      console.error('Error creating user:', error);
-      res.status(500).json({ error: 'Internal server error' });
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyASzODFRoEywWL42LyIFwxJbIxzQT2elRk",
+
+  authDomain: "eecs4413-6983c.firebaseapp.com",
+
+  databaseURL: "https://eecs4413-6983c-default-rtdb.firebaseio.com",
+
+  projectId: "eecs4413-6983c",
+
+  storageBucket: "eecs4413-6983c.appspot.com",
+
+  messagingSenderId: "236603067661",
+
+  appId: "1:236603067661:web:5e24337180d0eea066f444",
+
+  measurementId: "G-1CL3HY7CNK"
+
+};
+
+const app = initializeApp(firebaseConfig);
+//const db = getFirestore(app)
+const auth = getAuth(app)
+
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+	const userDoc = await req.app.locals.admin.firestore().collection('users').doc(user.uid).get();
+    const userData = userDoc.data();
+      res.status(200).json({ userData });
     }
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
   });
 
+ // intialize the DB of the user
 
- 
+	// to retrieve the dashboard of the user himself
+  
 
 module.exports = router;
